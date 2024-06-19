a1017be2a35699b7e8e438927439f8d639505e75
Spandana_Techie <169144373+spark8921@users.noreply.github.com>
2024-06-19 19:15:33 +0530

    Delete ApplicationLoadBalancer/alb directory

--- ApplicationLoadBalancer/alb/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "alb_dns_name" {
-  description = "DNS name of the APPLication Load Balancer"
-  value = aws_alb.alb.dns_name
-}
-
-output "alb_arn" {
-  description = "ARN of the application balancer"
-  value = aws_alb.alb.arn
-}
--- ApplicationLoadBalancer/alb/terraform.tfvars
+++
@@ -1,6 +0,0 @@
-region="ap-southeast-1"
-vpc_cidr = "10.0.0.0/16"
-subnet1_cidr = "10.0.1.0/24"
-subnet2_cidr = "10.0.2.0/24"
-web_instance_ami = "ami-003c463c8207b4dfa"
-web_instnace_type = "t2.micro"
